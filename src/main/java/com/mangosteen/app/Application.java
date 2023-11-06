727a9f6cfa25a31f6402adccb2feb550e12cd6e3
zhangge208 <zhangge208@gmail.com>
2023-11-05 18:40:56 -0800

    feat(docs): implement auto API document generation feature.

--- src/main/java/com/mangosteen/app/Application.java
+++ src/main/java/com/mangosteen/app/Application.java
@@ -1,9 +1,12 @@
 package com.mangosteen.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MangoSteen Service", version = "v1", description = "Backend APIs for MangoSteen App"))
 public class Application {
     // 入口
     public static void main(String[] args) {
--- src/main/java/com/mangosteen/app/controller/UserController.java
+++ src/main/java/com/mangosteen/app/controller/UserController.java
@@ -2,12 +2,17 @@ package com.mangosteen.app.controller;
 
 import com.mangosteen.app.mapper.UserInfoMapper;
 import com.mangosteen.app.model.UserInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "User APIs", description = "Related APIs for user management")
 public class UserController {
 
     private final UserInfoMapper userInfoMapper;
@@ -18,7 +23,13 @@ public class UserController {
     }
 
     @GetMapping("v1.0/users/{id}")
-    UserInfo getUserInfoById(@PathVariable("id") Long id) {
+    @Operation(summary = "Get user information", description = "Return the specific user information",
+    responses = {
+            @ApiResponse(responseCode = "200", description = "User information found"),
+            @ApiResponse(responseCode = "404", description = "User information not found")
+    })
+    UserInfo getUserInfoById(@Parameter(description = "The user id to fetch")
+                             @PathVariable("id") Long id) {
         return userInfoMapper.getUserInfoByUserId(id);
     }
 }
